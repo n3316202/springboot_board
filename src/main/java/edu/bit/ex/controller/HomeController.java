7026487550598c0f42b30b5675ec06ba33a60f06
nqwrt <nqwrt@ymail.com>
2021-02-05 00:16:42 +0900

    first commit

---
+++ src/main/java/edu/bit/ex/controller/HomeController.java
@@ -0,0 +1,14 @@
+package edu.bit.ex.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String home() {
+		return "Hello, Spring boot!";
+	}
+	
+}
---
+++ src/main/java/edu/bit/ex/vo/EmpVO.java
@@ -0,0 +1,23 @@
+package edu.bit.ex.vo;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class EmpVO {
+	int empno; // NOT NULL NUMBER(4)
+	String ename; // VARCHAR2(10)
+	String job;// VARCHAR2(9)
+	int mgr;// NUMBER(4)
+	Timestamp hiredate;// DATE
+	int sal;// NUMBER(7,2)
+	int comm;// NUMBER(7,2)
+	int deptno;// NUMBER(2)
+}
